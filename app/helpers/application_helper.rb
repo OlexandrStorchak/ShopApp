4acc75aa0aa0e32e63351956ce0c87c6a60006c0
Oleksandr <alexsstorchak@gmail.com>
2022-04-20 16:37:06 +0300

    Add helper for site title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def set_title_helper(title_name = "")
+    if title_name.present?
+       content_for :title, "#{title_name} | ShopApp"
+    else
+        content_for :title, "ShopApp"
+    end
+  end
+
+  def product_categories_list_helper
+    @categories = ProductCategory.all
+  end
 end
