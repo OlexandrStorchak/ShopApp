d05ba8b00085b9f69f9b303cbf8f8019b602f23f
Oleksandr <alexsstorchak@gmail.com>
2022-04-25 16:11:08 +0300

    fix user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,20 @@ class UsersController < ApplicationController
   end
 
   def log_in
-    user = User.find_by(email: params["email"])
-    if user.authenticate(params["password"])
-      redirect_to root_path
-      session[:login] = true
-      flash[:notice] = "Login"
+    if user = User.find_by(email: params["email"])
+      if user.authenticate(params["password"])
+        redirect_to root_path
+        session[:login] = true
+        flash[:notice] = "Login"
+      else
+        redirect_to users_path
+        session[:login] = false
+        flash[:error] = "Wrong password"
+      end
     else
       redirect_to users_path
       session[:login] = false
-      flash[:error] = "No login"
+      flash[:error] = "Wrong email"
     end
   end
 
